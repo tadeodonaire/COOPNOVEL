95ed7ce62934377c9d4175362328c9f851ab1e0e
Tadeo Donaire <116527582+tadeodonaire@users.noreply.github.com>
2025-04-12 21:04:25 -0500

    Initial commit

---
+++ src/main/java/pe/edu/upc/coopnovel/CoopnovelApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.coopnovel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoopnovelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoopnovelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/edu/upc/coopnovel/CoopnovelApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.coopnovel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoopnovelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
