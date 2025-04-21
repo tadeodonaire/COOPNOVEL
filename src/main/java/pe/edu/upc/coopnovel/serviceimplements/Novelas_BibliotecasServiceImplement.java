222a54d14f9f0098c9a6f84e709e602f0b9d8d6f
JhonAlex1997 <jhonal97.jacj@gmail.com>
2025-04-21 13:34:50 -0500

    Update Novelas_BibliotecasServiceImplement.java

--- src/main/java/pe/edu/upc/coopnovel/serviceimplements/Novelas_BibliotecasServiceImplement.java
+++ src/main/java/pe/edu/upc/coopnovel/serviceimplements/Novelas_BibliotecasServiceImplement.java
@@ -1,9 +1,8 @@
 package pe.edu.upc.coopnovel.serviceimplements;
 
 import org.springframework.stereotype.Service;
-import pe.edu.upc.coopnovel.entities.Novelas_Bibliotecas;
 import pe.edu.upc.coopnovel.serviceinterfaces.INovelas_BibliotecasService;
 @Service
-public abstract class Novelas_BibliotecasImplement implements INovelas_BibliotecasService {
+public abstract class Novelas_BibliotecasServiceImplement implements INovelas_BibliotecasService {
     
 }
