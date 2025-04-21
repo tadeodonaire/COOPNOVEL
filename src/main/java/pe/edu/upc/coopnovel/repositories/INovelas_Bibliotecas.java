1b789e48190f884e39c73508f169f7ed77fdd9ed
JhonAlex1997 <jhonal97.jacj@gmail.com>
2025-04-21 13:35:30 -0500

    Delete INovelas_Bibliotecas.java

--- src/main/java/pe/edu/upc/coopnovel/repositories/INovelas_Bibliotecas.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.upc.coopnovel.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pe.edu.upc.coopnovel.entities.Novelas_Bibliotecas;
-
-public interface INovelas_Bibliotecas extends JpaRepository <Novelas_Bibliotecas, Integer> {
-
-}
