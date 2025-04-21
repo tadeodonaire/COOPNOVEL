397f53e2b97ea3d2db528c9827889380808cccd6
JhonAlex1997 <jhonal97.jacj@gmail.com>
2025-04-21 17:12:09 -0500

    Update Novelas_BibliotecasServiceImplement.java

--- src/main/java/pe/edu/upc/coopnovel/serviceimplements/Novelas_BibliotecasServiceImplement.java
+++ src/main/java/pe/edu/upc/coopnovel/serviceimplements/Novelas_BibliotecasServiceImplement.java
@@ -1,8 +1,19 @@
 package pe.edu.upc.coopnovel.serviceimplements;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pe.edu.upc.coopnovel.entities.Novelas_Bibliotecas;
+import pe.edu.upc.coopnovel.repositories.INovelas_BibliotecasRepository;
 import pe.edu.upc.coopnovel.serviceinterfaces.INovelas_BibliotecasService;
+
+import java.util.List;
+
 @Service
 public abstract class Novelas_BibliotecasServiceImplement implements INovelas_BibliotecasService {
-    
+    @Autowired
+    private INovelas_BibliotecasRepository nBL;
+    @Override
+    public List<Novelas_Bibliotecas> list() {
+        return List.of();
+    }
 }
