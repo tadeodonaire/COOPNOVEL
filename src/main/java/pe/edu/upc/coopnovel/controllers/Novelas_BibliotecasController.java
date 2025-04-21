3d70e91a3749a0d719240995f0e78fa705ff19c7
JhonAlex1997 <jhonal97.jacj@gmail.com>
2025-04-21 18:00:42 -0500

    Update Novelas_BibliotecasController.java

--- src/main/java/pe/edu/upc/coopnovel/controllers/Novelas_BibliotecasController.java
+++ src/main/java/pe/edu/upc/coopnovel/controllers/Novelas_BibliotecasController.java
@@ -1,4 +1,16 @@
 package pe.edu.upc.coopnovel.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pe.edu.upc.coopnovel.entities.Novelas_Bibliotecas;
+import pe.edu.upc.coopnovel.serviceinterfaces.INovelas_BibliotecasService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/novelas_bibliotecas")
 public class Novelas_BibliotecasController {
+    private INovelas_BibliotecasService nBL;
+
+    
 }
