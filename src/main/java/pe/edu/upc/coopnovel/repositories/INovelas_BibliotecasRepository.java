dbc6294f9f49c197141637ab71618274485e3c0a
JhonAlex1997 <jhonal97.jacj@gmail.com>
2025-04-21 17:08:55 -0500

    Update INovelas_BibliotecasRepository.java

--- src/main/java/pe/edu/upc/coopnovel/repositories/INovelas_BibliotecasRepository.java
+++ src/main/java/pe/edu/upc/coopnovel/repositories/INovelas_BibliotecasRepository.java
@@ -1,4 +1,9 @@
 package pe.edu.upc.coopnovel.repositories;
 
-public interface INovelas_BibliotecasRepository      {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pe.edu.upc.coopnovel.entities.Novelas_Bibliotecas;
+@Repository
+public interface INovelas_BibliotecasRepository extends JpaRepository<Novelas_Bibliotecas, Integer> {
+
 }
