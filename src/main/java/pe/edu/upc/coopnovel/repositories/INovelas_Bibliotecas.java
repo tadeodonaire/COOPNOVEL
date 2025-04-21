8e82b6cc916e3692b28fab7335b09992b6ca1e95
JhonAlex1997 <jhonal97.jacj@gmail.com>
2025-04-20 19:52:01 -0500

    Create INovelas_Bibliotecas.java

---
+++ src/main/java/pe/edu/upc/coopnovel/repositories/INovelas_Bibliotecas.java
@@ -0,0 +1,8 @@
+package pe.edu.upc.coopnovel.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pe.edu.upc.coopnovel.entities.Novelas_Bibliotecas;
+
+public interface INovelas_Bibliotecas extends JpaRepository <Novelas_Bibliotecas, Integer> {
+
+}
