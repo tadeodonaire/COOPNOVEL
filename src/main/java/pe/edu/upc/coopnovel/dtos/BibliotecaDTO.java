97fd98040de0945650c477d9509d9649359c03dc
CustomStalker57 <149968754+CustomStalker57@users.noreply.github.com>
2025-04-22 11:49:59 -0500

    Add files via upload

---
+++ src/main/java/pe/edu/upc/coopnovel/dtos/BibliotecaDTO.java
@@ -0,0 +1,22 @@
+package pe.edu.upc.coopnovel.dtos;
+
+public class BibliotecaDTO {
+    private int idBiblioteca;
+    private String bibNombre;
+
+    public int getIdBiblioteca() {
+        return idBiblioteca;
+    }
+
+    public void setIdBiblioteca(int idBiblioteca) {
+        this.idBiblioteca = idBiblioteca;
+    }
+
+    public String getBibNombre() {
+        return bibNombre;
+    }
+
+    public void setBibNombre(String bibNombre) {
+        this.bibNombre = bibNombre;
+    }
+}
