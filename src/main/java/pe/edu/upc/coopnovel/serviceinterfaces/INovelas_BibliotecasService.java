8222be686face75e90947e2e5a592c4466b74713
JhonAlex1997 <jhonal97.jacj@gmail.com>
2025-04-27 20:10:36 -0500

    Eliminacion de tablas
    eliminacionde tablas mal creadas

--- src/main/java/pe/edu/upc/coopnovel/serviceinterfaces/INovelas_BibliotecasService.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upc.coopnovel.serviceinterfaces;
-
-import pe.edu.upc.coopnovel.entities.Novelas_Bibliotecas;
-
-import java.util.List;
-
-public interface INovelas_BibliotecasService {
-    public List<Novelas_Bibliotecas> list();
-}
