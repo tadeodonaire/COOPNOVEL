675ce9956cf4b47d36bf5b6118f79da2dc0ad2e0
JhonAlex1997 <jhonal97.jacj@gmail.com>
2025-04-27 20:10:43 -0500

    Delete INovelas_BibliotecasRepository.java

--- src/main/java/pe/edu/upc/coopnovel/repositories/INovelas_BibliotecasRepository.java
+++
@@ -1,6 +0,0 @@
-package pe.edu.upc.coopnovel.repositories;
-import pe.edu.upc.coopnovel.entities.Novelas_Bibliotecas;
-@Repository
-public interface INovelas_BibliotecasRepository extends JpaRepository<Novelas_Bibliotecas, Integer> {
-
-}
