979a20ca35c6655490eb9446b47804ceba3d2d34
ronalyp1705 <U20221C895@upc.edu.pe>
2025-04-29 12:21:37 -0500

    query capitulos descargados

--- src/main/java/pe/edu/upc/coopnovel/serviceinterfaces/ICapitulosService.java
+++ src/main/java/pe/edu/upc/coopnovel/serviceinterfaces/ICapitulosService.java
@@ -12,4 +12,5 @@ public interface ICapitulosService {
     public void delete (int id);
     public void update (Capitulos capitulos);
     public List<String[]> findCapituloByName(String titulo);
+    public List<String[]> quantityCapitulosbyUsuario();
 }
