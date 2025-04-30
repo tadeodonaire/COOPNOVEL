f1c89cec5139a5f61abc063d77236c97e810d678
CustomStalker57 <149968754+CustomStalker57@users.noreply.github.com>
2025-04-29 23:28:52 -0500

    Delete src/main/java/pe/edu/upc/coopnovel/repositories/IBibliotecaRepository.java

--- src/main/java/pe/edu/upc/coopnovel/repositories/IBibliotecaRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upc.coopnovel.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pe.edu.upc.coopnovel.entities.Biblioteca;
-
-@Repository
-public interface IBibliotecaRepository extends JpaRepository<Biblioteca, Integer> {
-}
