3adcbdb248bb4199bb831ea09bfa135757896ba8
Tadeo Donaire <116527582+tadeodonaire@users.noreply.github.com>
2025-04-30 14:27:23 -0500

    Ocultar Contraseña

---
+++ src/main/java/pe/edu/upc/coopnovel/dtos/UserSecurityDTO.java
@@ -0,0 +1,72 @@
+package pe.edu.upc.coopnovel.dtos;
+
+import pe.edu.upc.coopnovel.entities.Role;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class UserSecurityDTO {
+    private int idUsuario;
+    private String usNombre;
+    private String usApellido;
+    private LocalDate usFecNacimiento;
+    private String usCorreo;
+    private String username;
+    private List<Role> roles;
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getUsNombre() {
+        return usNombre;
+    }
+
+    public void setUsNombre(String usNombre) {
+        this.usNombre = usNombre;
+    }
+
+    public String getUsApellido() {
+        return usApellido;
+    }
+
+    public void setUsApellido(String usApellido) {
+        this.usApellido = usApellido;
+    }
+
+    public LocalDate getUsFecNacimiento() {
+        return usFecNacimiento;
+    }
+
+    public void setUsFecNacimiento(LocalDate usFecNacimiento) {
+        this.usFecNacimiento = usFecNacimiento;
+    }
+
+    public String getUsCorreo() {
+        return usCorreo;
+    }
+
+    public void setUsCorreo(String usCorreo) {
+        this.usCorreo = usCorreo;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
+    }
+}
