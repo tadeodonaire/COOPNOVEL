a5ff279299db7ec2ce3c0c1c291e84cd2990a1ea
Tadeo Donaire <116527582+tadeodonaire@users.noreply.github.com>
2025-05-09 19:17:02 -0500

    Autorización

--- src/main/java/pe/edu/upc/coopnovel/controllers/ProyectosController.java
+++ src/main/java/pe/edu/upc/coopnovel/controllers/ProyectosController.java
@@ -2,6 +2,7 @@ package pe.edu.upc.coopnovel.controllers;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import pe.edu.upc.coopnovel.dtos.NombreProyectosDTO;
 import pe.edu.upc.coopnovel.dtos.ProyectosDTO;
@@ -14,6 +15,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/proyectos")
+@PreAuthorize("hasAnyAuthority('ADMIN','USUARIO')")
 public class ProyectosController {
 
     @Autowired
