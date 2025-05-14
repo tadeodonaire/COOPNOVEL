2591dac964f9d0e878782a16c284c223b37e59af
u20211a679 <u20211a679@upc.edu.pe>
2025-05-14 11:04:57 -0500

    Entrega Semana 7 Correcciones de Queries de CorreccionesIA

--- src/main/java/pe/edu/upc/coopnovel/dtos/CorreccionesIADTO.java
+++ src/main/java/pe/edu/upc/coopnovel/dtos/CorreccionesIADTO.java
@@ -5,7 +5,7 @@ import pe.edu.upc.coopnovel.entities.Capitulos;
 public class CorreccionesIADTO {
     private int idCorreccionIA;
     private String corCorreccionIA;
-    private Capitulos capitulo;
+    private Capitulos capitulos;
 
     public int getIdCorreccionIA() {
         return idCorreccionIA;
@@ -23,11 +23,11 @@ public class CorreccionesIADTO {
         this.corCorreccionIA = corCorreccionIA;
     }
 
-    public Capitulos getCapitulo() {
-        return capitulo;
+    public Capitulos getCapitulos() {
+        return capitulos;
     }
 
-    public void setCapitulo(Capitulos capitulo) {
-        this.capitulo = capitulo;
+    public void setCapitulos(Capitulos capitulos) {
+        this.capitulos = capitulos;
     }
 }
