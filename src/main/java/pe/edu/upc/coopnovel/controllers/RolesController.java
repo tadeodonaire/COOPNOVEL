c7d4c4a96baabcf83dc12417694507af9c687170
Tadeo Donaire <116527582+tadeodonaire@users.noreply.github.com>
2025-07-03 16:28:04 -0500

    Version prueba, Registro Usuarios FRONT AL BACK
    Version prueba, Registro Usuarios FRONT AL BACK

--- src/main/java/pe/edu/upc/coopnovel/controllers/RolesController.java
+++ src/main/java/pe/edu/upc/coopnovel/controllers/RolesController.java
@@ -14,7 +14,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/roles")
-@PreAuthorize("hasAnyAuthority('ADMIN')")
+//@PreAuthorize("hasAnyAuthority('ADMIN')")
 public class RolesController {
 
     @Autowired
